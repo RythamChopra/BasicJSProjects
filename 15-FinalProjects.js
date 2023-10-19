cfa5592b8559663a848459b6fae22dbb131468b1
RythamChopra <145897285+RythamChopra@users.noreply.github.com>
2023-10-19 21:03:16 +0530

    Update 15-FinalProjects.js

--- 15-FinalProjects.js
+++ 15-FinalProjects.js
@@ -82,6 +82,8 @@ else if(result === 'you both tied'){
 
 document.querySelector('.choices')
 .innerHTML = `Your Pick : <img src = "${playerMove}-emoji.png">, Computer Pick : <img src = "${computerMove}-emoji.png"> = ${result}.`;
+
+    console.log(playerMove);
 document.querySelector('.js-score')
 .innerHTML = `Wins : ${score.wins} , Loss : ${score.loss} , Ties : ${score.ties}`;
 
